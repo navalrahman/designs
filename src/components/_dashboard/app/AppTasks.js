355f404d7e0900da49b134a21991a83f685de976
navalrahman <77826561+navalrahman@users.noreply.github.com>
2021-11-12 19:23:57 +0530

    Add files via upload

---
+++ src/components/_dashboard/app/AppTasks.js
@@ -0,0 +1,101 @@
+import {useState} from 'react'
+import PropTypes from 'prop-types';
+import { Form, FormikProvider, useFormik } from 'formik';
+// material
+import {
+  Box,
+  Card,
+  Checkbox,
+  CardHeader,
+  Typography,
+  FormControlLabel,
+  Stack
+} from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+const TASKS = [
+  'Create FireStone Logo',
+  'Add SCSS and JS files if required',
+  'Stakeholder Meeting',
+  'Scoping & Estimations',
+  'Sprint Showcase'
+];
+
+// ----------------------------------------------------------------------
+
+TaskItem.propTypes = {
+  task: PropTypes.string,
+  checked: PropTypes.bool,
+  formik: PropTypes.object
+};
+
+function TaskItem({ task, checked, formik, ...other }) {
+  const { getFieldProps } = formik;
+
+  return (
+    <Stack direction="row" justifyContent="space-between" sx={{ py: 0.75 }}>
+      <FormControlLabel
+        control={
+          <Checkbox {...getFieldProps('checked')} value={task} checked={checked} {...other} />
+        }
+        label={
+          <Typography
+            variant="body2"
+            sx={{
+              ...(checked && {
+                color: 'text.disabled',
+                textDecoration: 'line-through'
+              })
+            }}
+          >
+            {task}
+          </Typography>
+        }
+      />
+    </Stack>
+  );
+}
+
+export default function AppTasks() {
+    const [task,setTask] = useState('')
+
+  const formik = useFormik({
+    initialValues: {
+      checked: [TASKS[2]]
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
+
+  const { values, handleSubmit } = formik;
+
+  const handleChnage = (e) => {
+    const result = e.target.value
+    setTask(result)
+    console.log(result)
+  }
+
+  return (
+    <Card>
+      <CardHeader title="Tasks" />
+      <Box sx={{ px: 3, py: 1 }}>
+        <FormikProvider value={formik}>
+          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <input type="text" value={task} onChange={handleChnage}/><br />
+            <input type="button" value="AddTask"/>
+            {TASKS.map((task,i) => (
+              <TaskItem
+              key={i}
+                task={task}
+                formik={formik}
+                checked={values.checked.includes(task)}
+              />
+            ))}
+          </Form>
+        </FormikProvider>
+      </Box>
+    </Card>
+  );
+}
